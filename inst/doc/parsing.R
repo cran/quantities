9090a73bcd0730a46861777208fec8823f4caf60
Iñaki Ucar <iucar@fedoraproject.org>
2020-06-06 10:20:02 +0000

    version 0.1.4

--- inst/doc/parsing.R
+++ inst/doc/parsing.R
@@ -1,15 +1,15 @@
-## ---- cache = FALSE, include=FALSE---------------------------------------
+## ---- cache = FALSE, include=FALSE--------------------------------------------
 knitr::opts_chunk$set(collapse = T, comment = "#>", 
                       fig.width = 6, fig.height = 4, fig.align = "center")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 (d.quantities <- d.units <- d.errors <- read.csv(textConnection("
 quantities,        units,  errors
 1.02(5) g,         1.02 g, 1.02(5)
 2.51(0.01) V,      2.51 V, 2.51(0.01)
 (3.23 +/- 0.12) m, 3.23 m, 3.23 +/- 0.12"), stringsAsFactors=FALSE))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(quantities)
 
 for (name in names(d.quantities)) {
